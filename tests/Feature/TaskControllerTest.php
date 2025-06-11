0131272159c424e21c00ac218963a2616df68308
Vitor Meneguetti <vitor.meneguetti@gmail.com>
2025-06-11 07:45:17 -0300

    TDD list tasks

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -1,7 +1,18 @@
 <?php
 
-test('list tasks', function () {
-    $response = $this->get('api/tasks');
+use App\Models\Task;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
 
+uses(RefreshDatabase::class);
+
+test('list tasks', function () {
+    $numOfTasks = 3;
+    $tasks = Task::factory()->count(3)->create();
+
+    $response = $this->get('api/tasks');
     $response->assertStatus(200);
+    $response->assertJson(
+        fn (AssertableJson $json) => $json->has($numOfTasks)
+    );
 });
